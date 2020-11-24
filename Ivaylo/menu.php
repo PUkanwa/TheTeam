2473bf3f2328da7aa8a5a12c2fa5414b5dcb00ac
PUkanwa <73945731+PUkanwa@users.noreply.github.com>
2020-11-24 21:52:58 +0000

    Updated Menu

---
+++ Ivaylo/menu.php
@@ -0,0 +1,42 @@
+
+  
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    
+
+        <nav id="main-menu">
+            
+            <ul id="nav">
+                <li><a href="#"> <i class="fa fa-fw fa-home"></i> Home</a> </li>
+
+                <li><a href="#">Movies</a>
+                    
+                    <ul >
+                    <a class="dropdown-link" href="#">Upcoming Movies</a>
+                    <a class="dropdown-link" href="#">Trailers</a>
+                    <a class="dropdown-link" href="#">Recommendations</a>
+
+                    </ul>
+                    
+                </li>
+
+                <li><a href="#">Reviews</a> </li>
+
+
+                <li ><a href="#">Events</a></li>
+
+                <li><a href="#">Members</a></li>
+                
+                <li><a href="#">Join Us</a>
+                    
+                    <ul>
+                    <a class="dropdown-link" href="testimonial.php">Testimonials</a>
+                    <a class="dropdown-link" href="#">Join Now</a>
+                    </ul>
+
+                </li>
+
+                <li><a href="#">Contact us</a></li>
+                
+            </ul>
+            
+        </nav>
